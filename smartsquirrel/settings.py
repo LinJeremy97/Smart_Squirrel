1f12275d4da3c9e45263facbbbe9de545fb7fc1e
Jeremy Lin <jl5534@columbia.edu>
2019-12-08 16:11:18 +0000

    change to map name

--- smartsquirrel/settings.py
+++ smartsquirrel/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sightings',
-    'squirrel_app',
+    'squirrel_app.apps.SquirrelAppConfig',
 ]
 
 MIDDLEWARE = [
--- squirrel_app/apps.py
+++ squirrel_app/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class SquirrelAppConfig(AppConfig):
     name = 'squirrel_app'
+    verbose_name = 'map'
+    
