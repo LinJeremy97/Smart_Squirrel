c9a8e98423c65f0618175e14904fec17704f34cc
Jeremy Lin <jl5534@columbia.edu>
2019-12-08 16:33:34 +0000

    change filename to map

--- smartsquirrel/settings.py
+++ smartsquirrel/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sightings',
-    'squirrel_app.apps.SquirrelAppConfig',
+    'map',
 ]
 
 MIDDLEWARE = [
--- smartsquirrel/urls.py
+++ smartsquirrel/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('map/', include('squirrel_app.urls')),
+    path('map/', include('map.urls')),
     path('admin/', admin.site.urls),
     path('sightings/',include('sightings.urls')),
 ]
